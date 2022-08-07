320926d270afca19d6f8703e1bd3f6d102cab0aa
Mateusz Myszczyszyn <mateusz.myszczyszyn@vp.pl>
2022-08-07 21:56:35 +0200

    Add project files.

---
+++ MyBoards/Program.cs
@@ -0,0 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using MyBoards.Entities;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<MyBoardsContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("MyBoardsDbConnection")));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.Run();
+
