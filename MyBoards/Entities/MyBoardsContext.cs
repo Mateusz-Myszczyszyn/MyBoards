e35b054b06836956e024f9b510d76e847eea9cf6
Mateusz Myszczyszyn <mateusz.myszczyszyn@vp.pl>
2022-08-08 14:23:32 +0200

    Adding automaticly generated dates in sql server side and entity framework side.

--- MyBoards/Entities/MyBoardsContext.cs
+++ MyBoards/Entities/MyBoardsContext.cs
@@ -28,6 +28,13 @@ namespace MyBoards.Entities
                 eb.Property(wi => wi.EndDate).HasPrecision(3);
                 eb.Property(wi => wi.Activity).HasMaxLength(200);
                 eb.Property(wi => wi.RemaininWork).HasPrecision(14, 2);
+                eb.Property(wi => wi.Priority).HasDefaultValue(1);
+            });
+
+            modelBuilder.Entity<Comment>(eb =>
+            {
+                eb.Property(c => c.CreatedDate).HasDefaultValueSql("getutcdate()");
+                eb.Property(c => c.UpdatedDate).ValueGeneratedOnUpdate();
             });
                 
         }
